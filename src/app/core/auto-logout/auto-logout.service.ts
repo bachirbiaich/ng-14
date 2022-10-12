5b27c4b32e7575728d694132a14f62783c4437f2
bachirbiaich <bachir.biaich@gmail.com>
2022-10-12 21:52:02 +0200

    handle unsubscribe and deactivate

--- src/app/core/auto-logout/auto-logout.service.ts
+++ src/app/core/auto-logout/auto-logout.service.ts
@@ -4,11 +4,13 @@ import { AUTO_LOGOUT_CONFIG, AutoLogoutModule } from './auto-logout.module';
 import { IdleService } from './idle.service';
 import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
 import { AutoLogoutSbarComponent } from './auto-logout-sbar/auto-logout-sbar.component';
-import { take } from 'rxjs';
+import { Subject, take, takeUntil } from 'rxjs';
 
 @Injectable({ providedIn: AutoLogoutModule })
 export class AutoLogoutService {
   snackBarRef: MatSnackBarRef<AutoLogoutSbarComponent>;
+
+  private deactivate$ = new Subject<void>();
 
   constructor(
     @Inject(AUTO_LOGOUT_CONFIG) private config: AutoLogoutConfig,
@@ -23,12 +25,20 @@ export class AutoLogoutService {
         this.config.displayWarningBeforeTimeout ? this.config.warningWhenRemainSeconds : undefined
       );
       if (this.config.displayWarningBeforeTimeout && this.config.warningWhenRemainSeconds != null) {
-        this.idleService.warningBeforeExpired$.subscribe(() => this.onWarningBeforeExpired());
+        this.idleService.warningBeforeExpired$
+          .pipe(takeUntil(this.deactivate$))
+          .subscribe(() => this.onWarningBeforeExpired());
       }
-      this.idleService.expired$.subscribe(() => this.onExpired());
+      this.idleService.expired$.pipe(takeUntil(this.deactivate$)).subscribe(() => this.onExpired());
     } else {
       throw Error('AUTO_LOGOUT_CONFIG - timeoutSeconds cannot be null');
     }
+  }
+
+  deactivateAutoLogout(): void {
+    this.idleService.stopWatching();
+    this.deactivate$.next();
+    this.snackBarRef?.dismiss();
   }
 
   private onWarningBeforeExpired(): void {
@@ -36,13 +46,12 @@ export class AutoLogoutService {
     this.snackBarRef = this.snackBar.openFromComponent(AutoLogoutSbarComponent, {
       data: { counterSeconds: this.config.warningWhenRemainSeconds },
     });
-    this.idleService.idle$.pipe(take(1)).subscribe(() => this.snackBarRef.dismiss());
+    this.idleService.idle$.pipe(take(1), takeUntil(this.deactivate$)).subscribe(() => this.snackBarRef?.dismiss());
   }
 
   private onExpired(): void {
     console.log('onExpired');
-    this.idleService.stopWatching();
-    this.snackBarRef?.dismiss();
+    this.deactivateAutoLogout();
     // TODO : Logout Service call
   }
 }
